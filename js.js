df111668b8e624fc3e18d59d08b3c2acdce90519
Carlos Villatoro <66433870+Snake-oft@users.noreply.github.com>
2020-08-25 13:21:36 -0600

    mejora
    ases

--- js.js
+++ js.js
@@ -4,12 +4,11 @@ var boton4 = document.getElementById('boton4');
 
 function desplegarboton4() {
   botonlis4.classList.add('mostrar');
-
 };
 
 function ocultarboton4() {
   botonlis4.classList.remove('mostrar');
-
 };
 
 boton4.addEventListener("mouseover", desplegarboton4,true);
+boton4.addEventListener("mouseout", ocultarboton4,true);
