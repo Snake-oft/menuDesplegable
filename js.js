95a9be81292adc79b1c6299bfda016f22dac3f20
Carlos Villatoro <66433870+Snake-oft@users.noreply.github.com>
2020-08-25 11:02:31 -0600

    asi es
    ase ieas

---
+++ js.js
@@ -0,0 +1,10 @@
+var botonlis4 = document.getElementById('lisboton4');
+var boton4 = document.getElementById('boton4');
+
+
+function desplegarboton4() {
+  botonlis4.classList.add('mostrar');
+
+};
+
+boton4.addEventListener("click", desplegarboton4,true);
